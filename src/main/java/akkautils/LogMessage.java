8b7c7f54525683cc46aee22a0c6e01d50e30a55b
smurve <wgiersche@gmail.com>
2015-06-24 22:53:12 +0200

    The akka utils classes are now included

---
+++ src/main/java/akkautils/LogMessage.java
@@ -0,0 +1,25 @@
+package akkautils;
+
+import akka.actor.UntypedActor;
+
+/**
+ *  A log message 'knows' the sending actor
+ */
+public class LogMessage {
+
+    private final Class<? extends UntypedActor> source;
+    private final String message;
+
+    public LogMessage(Class<? extends UntypedActor> source, String message) {
+        this.source = source;
+        this.message = message;
+    }
+
+    public Class<? extends UntypedActor> getSource() {
+        return source;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+}
---
+++ src/main/java/akkautils/LoggerActor.java
@@ -0,0 +1,38 @@
+package akkautils;
+
+import akka.actor.Props;
+import akka.actor.UntypedActor;
+import akka.japi.Creator;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * asynchronously writes anything to the configured logger
+ */
+public class LoggerActor extends UntypedActor {
+
+    private static final Logger myLogger = LoggerFactory.getLogger(LoggerActor.class);
+
+    public static Props props() {
+        return Props.create(new Creator<LoggerActor>() {
+            private static final long serialVersionUID = 1L;
+
+            @Override
+            public LoggerActor create() throws Exception {
+                return new LoggerActor();
+            }
+        });
+    }
+
+    @Override
+    public void onReceive(Object message) throws Exception {
+
+        if ( message instanceof LogMessage ) {
+            LogMessage logMessage = (LogMessage) message;
+            Logger logger = LoggerFactory.getLogger(logMessage.getSource());
+            logger.info ( ((LogMessage)message).getMessage());
+        } else {
+            myLogger.info(message.toString());
+        }
+    }
+}
